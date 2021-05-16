c38645c033dbaf45381afa9cd21356c07d043b12
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-05-15 21:28:18 -0300

    First Model-Controller-View: Departament

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> departaments = new List<Departament>();
+
+            departaments.Add(new Departament { Id = 1, Name = "Eletronics" });
+            departaments.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(departaments);
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,8 @@
+namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
