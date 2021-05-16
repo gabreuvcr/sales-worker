84dfbb392079ffc2a6cf748bfab297363c3e74f9
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-05-16 01:51:39 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Departament departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+            Sales.Add(salesRecord);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            Sales.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sale => sale.Date >= initial && sale.Date <= final)
+                        .Sum(sale => sale.Amount);
+        }
+    }
+}
